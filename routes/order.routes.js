e8ef6f13538054482d8e298365b0ed0506d35ad7
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-12-02 19:09:53 +0100

    done

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,11 +1,28 @@
 import express from "express";
 import OrderModel from "../models/order.model.js";
+import ProductModel from "../models/product.model.js";
 
 const orderRoute = express.Router();
 
 orderRoute.post("/create-order", async (req, res) => {
   try {
     const newOrder = await OrderModel.create(req.body);
+
+    //console.log(newOrder);
+
+    //newOrder.products => [com todos os produtos] => [{productID: "", quantity: ""}]
+    newOrder.products.forEach(async (element) => {
+      await ProductModel.findByIdAndUpdate(
+        element.productID,
+        {
+          $push: {
+            orders: newOrder._id,
+          },
+        },
+        { runValidators: true }
+      );
+    });
+
     return res.status(201).json(newOrder);
   } catch (error) {
     console.error(error);
@@ -21,9 +38,17 @@ orderRoute.get("/order/:idOrder", async (req, res) => {
       populate: {
         path: "productID",
         model: "Product",
+        select: "name price -_id",
       },
     });
-    return res.status(200).json(order);
+
+    //total da order
+    let total = 0;
+    order.products.forEach((element) => {
+      total += element.productID.price * element.quantity;
+    });
+
+    return res.status(200).json({ order, total });
   } catch (error) {
     console.error(error);
     return res.status(500).json(error.errors);
